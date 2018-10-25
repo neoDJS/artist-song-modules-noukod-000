6eb1c99bd371a21ccdf7af0377256566f8ba3f28
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-25 15:23:50 +0000

    Done.

---
+++ lib/concerns/module_music.rb
@@ -0,0 +1,25 @@
+module Music
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+    
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+end
