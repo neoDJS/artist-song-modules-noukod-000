6a6612380ea9bd183c87b76c5938416f63f7a3e0
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-25 15:20:26 +0000

    automatically backed up by learn

--- lib/concerns/module_music.rb
+++ lib/concerns/module_music.rb
@@ -1,19 +1,22 @@
 module Music
   module ClassMethods
     def find_by_name(name)
-      #self.all.detect{|a| a.name == name}
+      self.all.detect{|a| a.name == name}
     end
 
     def reset_all
-      #self.all.clear
+      self.all.clear
     end
 
     def count
-      #self.all.count
+      self.all.count
     end
   end
 
   module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
     def to_param
       name.downcase.gsub(' ', '-')
     end
